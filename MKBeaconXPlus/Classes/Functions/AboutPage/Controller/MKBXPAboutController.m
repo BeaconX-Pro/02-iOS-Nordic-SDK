7b8dba945a606b074fdd031b44465ce949c4ad33
lovexiaoxia <aadyx2007@163.com>
2023-09-27 19:44:59 +0800

    wsgn

--- MKBeaconXPlus/Classes/Functions/AboutPage/Controller/MKBXPAboutController.m
+++ MKBeaconXPlus/Classes/Functions/AboutPage/Controller/MKBXPAboutController.m
@@ -116,7 +116,7 @@
         _versionLabel.textColor = RGBCOLOR(189, 189, 189);
         _versionLabel.textAlignment = NSTextAlignmentCenter;
         _versionLabel.font = MKFont(16.f);
-        _versionLabel.text = @"Version: V1.0.3";
+        _versionLabel.text = @"Version: V1.1.0";
     }
     return _versionLabel;
 }
