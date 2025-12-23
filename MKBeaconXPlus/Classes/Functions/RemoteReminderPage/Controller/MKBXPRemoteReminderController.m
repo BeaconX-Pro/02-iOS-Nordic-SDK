c26f283151602d70794023c4e1baf6b9514d12b1
lovexiaoxia <aadyx2007@163.com>
2025-12-23 14:38:28 +0800

    Fix

--- MKBeaconXPlus/Classes/Functions/RemoteReminderPage/Controller/MKBXPRemoteReminderController.m
+++ MKBeaconXPlus/Classes/Functions/RemoteReminderPage/Controller/MKBXPRemoteReminderController.m
@@ -313,7 +313,7 @@ MKBXPRemoteReminderCellDelegate>
     MKTextFieldCellModel *cellModel2 = [[MKTextFieldCellModel alloc] init];
     cellModel2.index = 1;
     cellModel2.msg = @"Blinking interval";
-    cellModel2.textPlaceholder = @"0~100";
+    cellModel2.textPlaceholder = @"1~100";
     cellModel2.textFieldValue = self.dataModel.blinkingInterval;
     cellModel2.textFieldType = mk_realNumberOnly;
     cellModel2.unit = @"x100ms";
